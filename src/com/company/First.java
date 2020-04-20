7740dfe55a89410ccc38c6f9ffce2b3fd22b482e
Arseny <st.arseny@yandex.ru>
2020-04-20 21:53:32 +0300

    comments added

--- src/com/company/First.java
+++ src/com/company/First.java
@@ -1,28 +1,68 @@
 package com.company;
 
+/**
+ * This class represents the first pack of tasks
+ * number in brackets (in commentaries) is a task No
+ * @author Stoyalov Arseny BVT1803
+ */
 public final class First {
 
-    //1
+    /**
+     * Returns remainder of the division
+     * (1)
+     * @param a first num
+     * @param b second num
+     * @return remainder
+     */
     public static int remainder(int a, int b) {
         return a % b;
     }
 
-    //2
+    /**
+     * Calculates area of a triangle
+     * (2)
+     * @param a triangle's side
+     * @param h triangle's height
+     * @return area
+     */
     public static double triArea(double a, double h) {
         return a * h / 2;
     }
 
-    //3
+    /**
+     * Counts all legs in herd
+     * (3)
+     * @param chick has 2 legs
+     * @param cow has 4 legs
+     * @param pig has 4 legs
+     * @return amount of legs
+     */
     public static int animals(int chick, int cow, int pig) {
         return chick * 2 + cow * 4 + pig * 4;
     }
 
-    //4
+    /**
+     * Checks if game is profitable
+     * (4)
+     * @param prob probability of winning
+     * @param prize amount of prize money
+     * @param pay how much money is required
+     * @return true if profitable
+     */
     public static boolean profitableGamble(double prob, double prize, double pay) {
         return prob * prize > pay;
     }
 
-    //5
+    /**
+     * Find out what operation needs to be
+     * applied so that first two params make
+     * the third
+     * (5)
+     * @param a of int
+     * @param b of int
+     * @param n of int
+     * @return String name of operation
+     */
     public static String operation(int a, int b, int n) {
         if (a + b == n) return "added";
         if (a - b == n) return "subtracted";
@@ -31,33 +71,67 @@ public final class First {
         return "none";
     }
 
-    //6
+    /**
+     * Returns ASCII code of given symbol
+     * (6)
+     * @param sym of char
+     * @return int code
+     */
     public static int ctoa(char sym) {
         return sym;
     }
 
-    //7
+    /**
+     * Takes a row of numbers, then picks last one
+     * and counts the sum of all numbers from 0 to
+     * the number picked
+     * (6)
+     * @param nums row of int
+     * @return sum
+     */
     public static int addUpTo(int... nums) {
+        if (nums.length == 0) return -1;
         int last = nums[nums.length - 1];
         int res = 0;
         for (int i = 1; i <= last; i++) res += i;
         return res;
     }
 
-    //8
+    /**
+     * The max length of third triangle edge
+     * (8)
+     * @param a first edge
+     * @param b second edge
+     * @return third edge
+     */
     public static int nextEdge(int a, int b){
         return a + b - 1;
     }
 
-    //9
+    /**
+     * Makes so that every element of array is
+     * replaced with its value powered by 3
+     * (9)
+     * @param array to proceed
+     * @return proceeded array
+     */
     public static int sumOfCubes(int[] array){
-        if (array == null) return 0;
+        if (array == null || array.length == 0) return 0;
         int res = 0;
         for (int e : array) res += Math.pow(e, 3);
         return res;
     }
 
-    //10
+    /**
+     * Add first arg to itself times second argument,
+     * then checks if the result can be divided by third
+     * arg
+     * (10)
+     * @param a first
+     * @param b second
+     * @param c third
+     * @return true if the result of division is integer
+     */
     public static boolean abcmath(int a, int b, int c){
         for (int i = 0; i < b; i++) {
             a = a + a;
--- src/com/company/Second.java
+++ src/com/company/Second.java
@@ -3,9 +3,20 @@ package com.company;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * This class represents the second pack of tasks
+ * number in brackets (in commentaries) is a task No
+ * @author Stoyalov Arseny BVT1803
+ */
 public final class Second {
 
-    //1
+    /**
+     * Method repeats each each letter in a word n times
+     * (1)
+     * @param word that will have repeated letters
+     * @param times to repeat
+     * @return result as String
+     */
     public static String repeat(String word, int times){
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < word.length(); i++) {
@@ -14,7 +25,13 @@ public final class Second {
         return builder.toString();
     }
 
-    //2
+    /**
+     * Finds smallest and largest numbers in array
+     * and subtract one from another
+     * (2)
+     * @param array to proceed
+     * @return difference between min and max
+     */
     public static int differenceMaxMin(int[] array){
         int max = 0;
         int min = 0;
@@ -25,14 +42,25 @@ public final class Second {
         return max - min;
     }
 
-    //3
+    /**
+     * Checks if average value of array elements is integer
+     * (3)
+     * @param array to proceed
+     * @return true if integer
+     */
     public static boolean isAvgWhole(int[] array){
         int sum = 0;
         for (int a : array) sum += a;
         return sum % array.length == 0;
     }
 
-    //4
+    /**
+     * Takes array and makes its elements equal to sum
+     * of all previous elements
+     * (4)
+     * @param array to proceed
+     * @return array where element is the sum of previous ones
+     */
     public static int[] cumulativeSum(int[] array){
         int sum = 0;
         for (int i = 0; i < array.length; i++) {
@@ -42,7 +70,13 @@ public final class Second {
         return array;
     }
 
-    //5
+    /**
+     * Finds out how many digits to the right of the
+     * decimal point there are in the number
+     * (5)
+     * @param num String value of number
+     * @return amount of digit
+     */
     public static int getDecimalPlaces(String num){
         try {
             double n = Double.parseDouble(num);
@@ -54,7 +88,13 @@ public final class Second {
         return decimals.length();
     }
 
-    //6
+    /**
+     * Returns an element of the Fibonacci row at
+     * a certain spot
+     * (6)
+     * @param num number of element to search
+     * @return element
+     */
     public static long Fibonacci(int num){
         long first = 1;
         long second = 1;
@@ -66,31 +106,66 @@ public final class Second {
         return second;
     }
 
-    //7
+    /**
+     * Checks if post index is valid i.e. is no longer than 5
+     * digits and contains digits only
+     * (7)
+     * @param index String value of index to proceed
+     * @return true if valid
+     */
     public static boolean isValid(String index){
-        if (index.length() > 5) return false;
+        if (index.length() > 5 || index.length() == 0) return false;
         Matcher matcher = Pattern.compile("[\\D]+").matcher(index);
         return !matcher.find();
     }
 
-    //8
+    /**
+     * Checks if first word starts with a letter that is the last
+     * letter os second word and vice versa
+     * (8)
+     * @param first word
+     * @param second word
+     * @return if the condition is met
+     */
     public static boolean isStrangePair(String first, String second){
         return first.charAt(0) == second.charAt(second.length() - 1)
                 && first.charAt(first.length() - 1) == second.charAt(0);
     }
 
-    //9
+    /**
+     * Checks if word has the given prefix
+     * (9)
+     * @param word to check
+     * @param prefix to check
+     * @return true if has
+     */
     public static boolean isPrefix(String word, String prefix){
         if (prefix.charAt(prefix.length() - 1) == '-') prefix = prefix.substring(0, prefix.length() - 1);
         return prefix.equals(word.substring(0, prefix.length()));
     }
 
+    /**
+     * Checks if word has the given suffix
+     * (9)
+     * @param word to check
+     * @param suffix to check
+     * @return true if has
+     */
     public static boolean isSuffix(String word, String suffix){
         if (suffix.charAt(0) == '-') suffix = suffix.substring(1);
         return suffix.equals(word.substring(word.length() - suffix.length()));
     }
 
-    //10
+    /**
+     * Returns the amount of blocks produced
+     * at a certain stage of an algorithm.
+     * At the first stage three blocks appear,
+     * at the second one disappears, then
+     * it all loops
+     * (10)
+     * @param step at which blocks will be produced
+     * @return amount of blocks
+     */
     public static int boxSeq(int step){
         switch(step % 2){
             case 1: return step + 2;
